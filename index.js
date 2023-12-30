6b4ca694e76bb2ede0da04930a1bca81f8b15de4
Raj <tiwariraj91929@gmail.com>
2023-12-30 22:15:54 +0530

    added click listners to button

--- index.js
+++ index.js
@@ -64,6 +64,8 @@
     
     
 //     } 
+    
+    
 //     let player = 0
 //     let comp = 0
 //     console.log(game());
@@ -71,3 +73,88 @@
 //     console.log(game());
 //     console.log(game());
 //     console.log(game());
+
+
+
+function getComputerChoice() {
+           let a= Math.floor(Math.random()*3)
+           if(a==1){
+             return "rock"
+           }else if (a==2){
+             return "paper"
+           }else{
+             return "scissors"
+           }
+         }
+
+
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+
+      // and for each one we add a 'click' listener
+      button.addEventListener('click', () => {
+        let playerSelection = button.className;
+        const computerSelection = getComputerChoice();
+
+
+
+
+        let player = 0
+       let comp = 0
+
+
+// function game(){
+
+            function playRound(playerSelection, computerSelection) {
+      if (playerSelection == computerSelection){
+         return "It's a tie";
+       } else if(computerSelection== "rock"){
+         if(playerSelection=="paper"){
+           player = ++player;
+           comp= comp;
+           return "You win! paper beats rock";
+         }else{
+           player =player;
+           comp= ++comp;
+           return "You lose! rock beats scissors"
+         }
+       } else if(computerSelection== "paper"){
+         if(playerSelection=="scissors"){
+           player = ++player;
+            comp= comp;
+           return "You win! scissors beats paper";
+         }else{
+           player = player;
+            comp= ++comp;
+           return "You lose! paper beats rock";
+         }
+       } else if(computerSelection== "scissors"){
+         if(playerSelection=="rock"){
+           player = ++player;
+            comp=comp;
+           return "You win! rock beats scissors";
+         }else{
+           player = player;
+            comp= ++comp;
+           return "You lose! scissors beats paper";
+         }
+       } 
+    
+    }
+
+
+ // }
+
+
+
+
+
+
+
+
+
+        console.log(playRound(computerSelection,playerSelection));
+        
+      });
+    });
